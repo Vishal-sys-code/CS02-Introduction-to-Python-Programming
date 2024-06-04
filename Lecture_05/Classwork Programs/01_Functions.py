1ce4beb3c80c01d024360538f1877b199a2a7ac6
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-04 15:33:48 +0530

    Create 01_Functions.py

---
+++ Lecture_05/Classwork Programs/01_Functions.py
@@ -0,0 +1,13 @@
+def hello():
+    print("Hello")
+hello()
+
+def  hello_1(name):
+    print("Hello " + name)
+    print("Have a nice day")
+hello_1("Annie")
+
+def hello_2(firstname, lastname, age):
+    print("Hello " + firstname + " " + lastname + " " + str(age))
+    print("Have a nice day")
+hello_2("Annie", "Sikdar", 23)
