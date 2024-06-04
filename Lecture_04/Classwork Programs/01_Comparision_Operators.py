49afd2e88e9fa23053cb768b62631c76c952463d
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-04 09:14:04 +0530

    Create 01_Comparision_Operators.py

---
+++ Lecture_04/Classwork Programs/01_Comparision_Operators.py
@@ -0,0 +1,14 @@
+# Comparision Operators
+x = 5
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than equal to 5')
+if x < 6:
+    print('Less than 6')
+if x <= 5:
+    print('Less than equal to 5')
+if x != 6:
+    print('Not equal to 6')
