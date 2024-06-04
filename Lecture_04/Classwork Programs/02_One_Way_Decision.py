8bb0de1d786cc73b5d543c30c69ee5a6553f1a88
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-04 09:18:43 +0530

    Create 02_One_Way_Decision.py

---
+++ Lecture_04/Classwork Programs/02_One_Way_Decision.py
@@ -0,0 +1,14 @@
+# ONE WAY DECISION
+x = 5
+print('Before 5')
+if x == 5:
+    print('Is 5')
+    print('Is still 5')
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x == 6:
+    print('Is 6')
+    print('Is Still 6')
+    print('Third 6')
+print('Afterwards 6')
