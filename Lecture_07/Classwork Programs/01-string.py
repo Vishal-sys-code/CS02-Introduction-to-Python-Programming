20ea6283037e5dd808e71160bdb6f7095ab1f5a5
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-07 15:36:46 +0530

    added

---
+++ Lecture_07/Classwork Programs/01-string.py
@@ -0,0 +1,18 @@
+# Creating string with a single quotes
+string1 = 'Welcome to the Cambridge University'
+print("String with the use of Single Quotes: ")
+print(string1)
+
+string2 = "I'm a Professor"
+print("\nString with the use of Double Quotes: ")
+print(string2)
+
+string3 = '''I'm a part-time professor who comes to show the world of computer science'''
+print("\nString with the use of the Triple Quotes: ")
+print(string3)
+
+string4 = '''I am 
+            a Machine Learning Engineer
+            and your mentor'''
+print("\nCreating a multiline String: ")
+print(string4)
