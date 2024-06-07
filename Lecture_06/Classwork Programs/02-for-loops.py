7fb3484b1defe6e1d2e045948121da7bf2f0454c
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-07 09:35:59 +0530

    Update 02-for-loops.py

--- Lecture_06/Classwork Programs/02-for-loops.py
+++ Lecture_06/Classwork Programs/02-for-loops.py
@@ -20,3 +20,32 @@ for seconds in range (10,0,-1):
     print(seconds)
     time.sleep(1)
 print("Konichiwa")
+
+print("------- Iteration of the Data Structures -------")
+
+print("------- List Iteration -------")
+list = ["Lana", "Del", "Ray"]
+for i in list:
+    print(i)
+
+print("------- Tuple Iteration -------")
+tuple = ("Lana", "Del", "Ray")
+for i in tuple:
+    print(i)
+
+print("------- String Iteration -------")
+str = "Lana Del Ray"
+for i in str:
+    print(i)
+
+print("------- Dictionary Iteration -------")
+d = dict()
+d['abc'] = 123
+d['xyz'] = 456
+for i in d:
+    print("%s %d" % (i, d[i]))
+
+print("------- Set Iteration -------")
+set1 = {1,2,3,4,5,6}
+for i in set1:
+    print(i)
