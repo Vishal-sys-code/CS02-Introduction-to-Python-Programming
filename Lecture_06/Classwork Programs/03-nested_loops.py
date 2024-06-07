5e1df374d74b8df9bc2b2422ccd55d336b8cffbe
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-07 09:54:47 +0530

    Update 03-nested_loops.py

--- Lecture_06/Classwork Programs/03-nested_loops.py
+++ Lecture_06/Classwork Programs/03-nested_loops.py
@@ -1,8 +1,8 @@
 row = int(input("Enter the Rows: "))
-cols = int(input("Enter the Columns :"))
-symbol = input("Enter the symbol :")
+cols = int(input("Enter the Columns: "))
+symbol = input("Enter the symbol: ")
 
 for i in range(row):
     for j in range(cols):
-        print(symbol)
+        print(symbol, end = " ")
     print()
