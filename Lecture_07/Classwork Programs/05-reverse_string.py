c53c1e6b0878cc8e898b32288d24d7a5b342dc05
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-08 09:13:10 +0530

    Create 05-reverse_string.py

---
+++ Lecture_07/Classwork Programs/05-reverse_string.py
@@ -0,0 +1,10 @@
+name_1 = "Elon Musk"
+name_2 = "ksuM nolE"
+name_3 = "Andrew Huberman"
+name_4 = "namrebuH werdnA"
+
+print("----- Reversing the strings -----")
+print("Reversed name_1: " + name_1[::-1])
+print("Reversed name_2: " + name_2[::-1])
+print("Reversed name_3: " + name_3[::-1])
+print("Reversed name_4: " + name_4[::-1])
