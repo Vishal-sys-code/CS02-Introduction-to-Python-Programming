02315b64c17e5d7a3bb29fd4c0bddbb7ef31cc58
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-08 09:08:20 +0530

    Create 04-string_slicing.py

---
+++ Lecture_07/Classwork Programs/04-string_slicing.py
@@ -0,0 +1,57 @@
+name = "Lana Del Ray"
+
+first_name = name[0]
+print(first_name)
+
+first_name_i1 = name[0:1]
+first_name_i2 = name[0:2]
+first_name_i3 = name[0:3]
+first_name_i4 = name[0:4]
+first_name_i5 = name[0:5]
+first_name_i6 = name[0:6]
+first_name_i7 = name[0:7]
+first_name_i8 = name[0:8]
+first_name_i9 = name[0:9]
+first_name_i10 = name[0:10]
+first_name_i11 = name[0:11]
+first_name_i12 = name[0:12]
+
+print("---- Indexing: (where start is 0)----")
+print("first_name_i1: " + first_name_i1)
+print("first_name_i2: " + first_name_i2)
+print("first_name_i3: " + first_name_i3)
+print("first_name_i4: " + first_name_i4)
+print("first_name_i5: " + first_name_i5)
+print("first_name_i6: " + first_name_i6)
+print("first_name_i7: " + first_name_i7)
+print("first_name_i8: " + first_name_i8)
+print("first_name_i9: " + first_name_i9)
+print("first_name_i10: " + first_name_i10)
+print("first_name_i11: " + first_name_i11)
+print("first_name_i12: " + first_name_i12)
+
+print("----- Indexing: (where start and end both will be taken) -----")
+first_name_j1 = name[1:2]
+first_name_j2 = name[2:3]
+first_name_j3 = name[3:4]
+first_name_j4 = name[4:5]
+first_name_j5 = name[5:6]
+first_name_j6 = name[7:8]
+first_name_j7 = name[8:9]
+
+print("first_name_j1: " + first_name_j1)
+print("first_name_j2: " + first_name_j2)
+print("first_name_j3: " + first_name_j3)
+print("first_name_j4: " + first_name_j4)
+print("first_name_j5: " + first_name_j5)
+print("first_name_j6: " + first_name_j6)
+print("first_name_j7: " + first_name_j7)
+
+print("----- Having fun with slicing -----")
+funky_name_1 = name[0:8:1]
+funky_name_2 = name[0:8:2]
+funky_name_3 = name[0:8:3]
+
+print("funky_name_1: " + funky_name_1)
+print("funky_name_2: " + funky_name_2)
+print("funky_name_3: " + funky_name_3)
