678fc889b90d293d28e1633b0a0759865ca14583
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-12 20:37:09 +0530

    added

---
+++ Lecture_08/Classwork Programs/01-read_mode.py
@@ -0,0 +1,7 @@
+import os
+# The file "file.txt", will be opened with the reading mode
+file = open(r'C:\Users\bisha\OneDrive\Desktop\Workplace\CS02-Introduction-to-Python-Programming\Lecture_08\Classwork Programs\file.txt', 'r')
+
+# Printing each line one by one in the file
+for each in file:
+    print(each)
