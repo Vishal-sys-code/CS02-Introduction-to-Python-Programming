0f4a650c8cd8a621f25b83d46e44ffd2ed97df97
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-14 17:08:49 +0530

    written into the file with the help of write method

--- Lecture_08/Classwork Programs/02-write_mode.py
+++ Lecture_08/Classwork Programs/02-write_mode.py
@@ -1,1 +1,5 @@
-file = open()
+file = open('write_mode_file.txt', 'w')
+file.write("Hi, This is the write command")
+file.write("Nice to meet you")
+file.write("It allows us to write in a particular file")
+file.close()
