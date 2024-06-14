ebcb838f25fb5d6042d18839ac84754b75f6a668
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-14 17:03:32 +0530

    changed the txt files name

--- Lecture_08/Classwork Programs/01-read_mode.py
+++ Lecture_08/Classwork Programs/01-read_mode.py
@@ -3,23 +3,23 @@ import os
 print("------ File opening in Reading mode ------")
 # The file "file.txt", will be opened with the reading mode
 print("--- Example 1 ---")
-file = open('file.txt', 'r')
+file = open('read_mode_file.txt', 'r')
 # Printing each line one by one in the file
 for each in file:
     print(each)
 
 print()
 print("--- Example 2 ---")
-file = open('file.txt', 'r')
+file = open('read_mode_file.txt', 'r')
 print(file.read())
 
 print()
 print("--- Example 3 ---")
-with open("file.txt") as file:  
+with open("read_mode_file.txt") as file:  
     data = file.read() 
 print(data)
 
 print()
 print("--- Example 4 ---")
-file = open('file.txt','r')
+file = open('read_mode_file.txt','r')
 print(file.read(5))
