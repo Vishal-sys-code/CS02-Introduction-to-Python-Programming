7a84d58603c5f8c3a732f567402f6749061a249b
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-15 09:59:03 +0530

    Edited write_mode.py

--- Lecture_08/Classwork Programs/02-write_mode.py
+++ Lecture_08/Classwork Programs/02-write_mode.py
@@ -1,5 +1,5 @@
 # Example - 1 
-
+ 
 print("Example - 1")
 file = open('write_mode_file.txt', 'w')
 file.write("Hi, This is the write command")
@@ -7,4 +7,8 @@ file.write("Nice to meet you")
 file.write("It allows us to write in a particular file")
 file.close()
 
+
 # Example - 2
+print("Example - 2")
+with open('write_mode_file.txt','w') as f:
+    f.write("Hi, I am written statement along with the with() method or statement")
