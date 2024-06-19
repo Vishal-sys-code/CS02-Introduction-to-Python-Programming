951fedc2fbb358d6b2dca35eb0f6c64741d371ea
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-19 16:06:50 +0530

    Create 01-creating_basic_list.py

---
+++ Lecture_09/Classwork Programs/01-creating_basic_list.py
@@ -0,0 +1,11 @@
+list = []
+print("Empty List: ") 
+print(list)
+
+list = [10, 11, 12]
+print("\nList with numbers: ")
+print(list)
+
+list = ["Stanford", "University"]
+print("\nList with Strings: ")
+print(list)
