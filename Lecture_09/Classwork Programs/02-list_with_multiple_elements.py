a4b20792ef890afff04072c69262587220310337
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-19 17:00:51 +0530

    Create 02-list_with_multiple_elements.py

---
+++ Lecture_09/Classwork Programs/02-list_with_multiple_elements.py
@@ -0,0 +1,21 @@
+list = [1,2,2,3,3,3,4,5,6,6,7,8,9,9,10,10]
+print("List containing duplicate value: ")
+print(list)
+
+list = [3,4,"Stanford",2,0,1,"University"]
+print("List containing mixed values: ")
+print(list)
+
+# To get the access of the list
+print("\nAccessing the Lists: ")
+print(list[0])
+print(list[1])
+
+# Negative Indexing
+print("\nDisplaying the Negative Indexing: ")
+print(list[-1])
+print(list[-3])
+
+# Getting size of list
+print("\nDisplaying the Length of the List: ")
+print(len(list))
