bdd1abae2c090828cd8ba9c3b153e161ac732c26
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-19 17:09:39 +0530

    Update 03-user_input_in_list.py

--- Lecture_09/Classwork Programs/03-user_input_in_list.py
+++ Lecture_09/Classwork Programs/03-user_input_in_list.py
@@ -23,3 +23,14 @@ for i in range(0,n):
     element = [input(), int(input())]
     lst.append(element)
 print(lst)
+
+# There are two in-built functions through which we can insert the elements at the end of the list, the functions are:- `append()` and `extend()`
+list = []
+list.append(1)
+list.append(2)
+list.append(3)
+for i in range(1,4):
+    list.append(i)
+
+list2 = ["Fydor", "Dostoevsky"]
+list.append(list2) # Adding list in a list
