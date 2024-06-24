d416e882b5fe7edfa868ef8fae92939065565ea9
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-24 20:34:07 +0530

    Update 05-removing_item_in_list.py

---
+++ Lecture_09/Classwork Programs/05-removing_item_in_list.py
@@ -0,0 +1,19 @@
+print("------- Method 1: Using remove() method -------")
+list = [1,2,3,4,5,6,7,8,9,10]
+print("Initial List: ")
+print(list)
+list.remove(5)
+list.remove(3)
+print("\nList after removing two elements: ")
+print(list)
+
+list1 = [1,2,3,4,5,6,7,8,9,10,11,12]
+print("Initial List: ")
+print(list1)
+for i in range(1,5):
+    list1.remove(i)
+print("\nList after removing two elements: ")
+print(list1)
+
+print("-----------------------------------------------")
+print("")
