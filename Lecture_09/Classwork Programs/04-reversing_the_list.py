ed70af7f858bf0fbf55cd25256b7b91f6ef4f627
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-24 08:34:29 +0530

    Update 04-reversing_the_list.py

--- Lecture_09/Classwork Programs/04-reversing_the_list.py
+++ Lecture_09/Classwork Programs/04-reversing_the_list.py
@@ -1,3 +1,16 @@
+print("------ Example 1: Using reverse() method ------")
 mylist = [1,2,3,4,5,'Meta', 'Microsoft']
 mylist.reverse()
 print(mylist)
+
+print("------ Example 2: Using reversed() function ------")
+
+"""
+NOTES: 
+The `reversed()` function returns a reverse iterator, 
+which can be converted to a lost using the `list()` 
+function.
+"""
+mylist = [1,2,3,4,5]
+reversed_list = list(reversed(mylist))
+print(reversed_list)
