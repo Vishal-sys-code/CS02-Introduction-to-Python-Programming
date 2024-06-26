57215f5edb4925d542388aaf40c719236d71f3cb
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-26 17:51:15 +0530

    Create 05-deleting_elements_in_dictionary.py

---
+++ Lecture_10/ClassWork Programs/05-deleting_elements_in_dictionary.py
@@ -0,0 +1,7 @@
+# Deleting elments using 'del' keyword
+dict = {1:'Andrew', 2:'Huberman', 3: 'Podcasts'}
+print("Initial Dictionary: ")
+print(dict)
+del(dict[1])
+print("Displaying dictionary after deletion: ")
+print(dict)
