7c0e0790d870b156ca000f5cd53278d8249a2801
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-26 09:06:45 +0530

    renamed and added

---
+++ Lecture_10/ClassWork Programs/02-nested_dictionary.py
@@ -0,0 +1,25 @@
+dict_1 = {1:'Geeks', 2:'For', 3:{'A':'Welcome', 'B':'To', 'C':'Introvert Club'}}
+
+dict_2 = {
+    1:'Geeks', 
+    2:'For', 
+    3:{
+        'A':'Welcome', 
+        'B':'To', 
+        'C':'Introvert Club'
+    }
+}
+
+dict_3 = {
+    1:'Geeks',
+    2:'Nerds',
+    3:{
+        'A':'Good',
+        'B':'Morning',
+        'C':'Stanford'
+    }
+}
+
+print(dict_1)
+print(dict_2)
+print(dict_3)
