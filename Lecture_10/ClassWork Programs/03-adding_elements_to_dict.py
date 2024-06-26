844a4202af05b2928f8a9a922b535925f925da42
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-26 09:14:25 +0530

    adding and renamed

---
+++ Lecture_10/ClassWork Programs/03-adding_elements_to_dict.py
@@ -0,0 +1,22 @@
+dict = {}
+print("Empty Dictionary")
+print(dict)
+
+dict[0] = 'Geeks'
+dict[1] = 'Nerds'
+dict[3] = 100
+
+print("\n After adding 3 elements")
+print(dict)
+
+dict['value_set'] = 2,3,4
+print("\n After adding 3 elements")
+print(dict)
+
+dict[2] = "Welcome"
+print("\nUpdate key Value")
+print(dict)
+
+dict[5] = {'Nested':{1:'Life', 2:'Introverts'}}
+print("\n Adding a Nested Key")
+print(dict)
