7d75f0c04fe23b14402db24edddfe14575ce026a
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-26 10:26:06 +0530

    changed the files
    changed the python files and also edited the markdown of the lecture fukes.

--- Lecture_10/ClassWork Programs/04-accessing_elements.py
+++ Lecture_10/ClassWork Programs/04-accessing_elements.py
@@ -8,3 +8,13 @@ print("\nAccessing element using key")
 print(dict['name'])
 print("\nAccessing a element using key")
 print(dict[1])
+
+# Using get() method
+
+dict = {
+    1: 'Andrew',
+    2:'Huberman',
+    3:'Podcasts'
+}
+print("\nAccessing element using key")
+print(dict.get(2))
