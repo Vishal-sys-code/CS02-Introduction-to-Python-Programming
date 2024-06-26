c122f542c628c798ded408183287382d6a8beb77
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-26 09:20:54 +0530

    Create 04-accessing_elements.py

---
+++ Lecture_10/ClassWork Programs/04-accessing_elements.py
@@ -0,0 +1,10 @@
+dict = {
+    1:'Geeks',
+    'name': 'GH',
+    3:'Hardy'
+}
+
+print("\nAccessing element using key")
+print(dict['name'])
+print("\nAccessing a element using key")
+print(dict[1])
