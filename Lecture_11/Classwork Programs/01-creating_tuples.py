09399801def7a04ffaa03831233cfbe5646c813a
Vishal Pandey <bishalpandey2001@gmail.com>
2024-06-27 08:51:04 +0530

    Edited the Tutorial

---
+++ Lecture_11/Classwork Programs/01-creating_tuples.py
@@ -0,0 +1,10 @@
+# Creating Tuple using Round Brackets
+var = ("Sachin", "Mahi", "Yuvraj")
+print(var)
+print(type(var))
+
+# Creating a Tuple with one item
+mytuple = ("Advertisement",)
+print(type(mytuple))
+mytuple = ("Advertisement")
+print(type(mytuple))
