4a13a11c242ef8cbabb442c111dc4a3db21d58f4
Vishal Pandey <bishalpandey2001@gmail.com>
2024-10-14 15:42:02 +0530

    Added-Exceptional-Handling

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-set1 = {1,2,3,4,5,6,7,8}
-print("Initial Set: ")
-print(set1)
-set1.clear()
-print("Set after using clear() function")
-print(set1)
+try:
+    raise NameError("Hi there!!!")
+except NameError:
+    print("An error occurred!!!")
+    raise
+
+"""
+Output: 
+py", line 2, in <module>
+    raise NameError("Hi there!!!")
+NameError: Hi there!!!
+"""
