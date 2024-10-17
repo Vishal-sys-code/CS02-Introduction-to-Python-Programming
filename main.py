ad6e3dfa6f7f0918999a6bdf63d404c417974e77
Vishal Pandey <bishalpandey2001@gmail.com>
2024-10-17 16:27:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
-class Car:
-    name = None
-class motorCycle:
-    name = None
-def changeColor(Car, name):
-    car.name = name
-def changeColor(motorCycle, name):
-    motorCycle.name = name
+class Duck:
+    def walk(self):
+        print("Duck is walking")
+    def talk(self):
+        print("Duck is quackng")
+class Person:
+    def catch(self, Duck):
+        Duck.walk()
+        Duck.talk()
+        print("You catch me")
+class Chicken:
+    def walk(self):
+        print("The chicken is walking")
+    def talk(self):
+        print("The chicken is talking")
 
-car_1 = Car()
-car_2 = Car()
-car_3 = Car()
-changeColor(car_1, "Lamborghini")
-changeColor(car_2, "Ferrari")
-changeColor(car_3, "McLaren")
-
-bike_1 = motorCycle()
-bike_2 = motorCycle()
-bike_3 = motorCycle()
-changeColor(bike_1, "Duccati")
-changeColor(bike_2, "Hayabusa")
-changeColor(bike_3, "Himalayan Bullet")
-
-print("Car Details: ")
-print(car_1.name)
-print(car_2.name)
-print(car_3.name)
-print("Bike Details: ")
-print(bike_1.name)
-print(bike_2.name)
-print(bike_3.name)
+duck = Duck()
+chicken = Chicken()
+person = Person()
+person.catch(duck)
+person.catch(chicken)
 
 """
 Output:
-Car Details: 
-Lamborghini
-Ferrari
-McLaren
-Bike Details:
-Duccati
-Hayabusa
-Himalayan Bullet
+Duck is walking
+Duck is quackng
+You catch me
+The chicken is walking
+The chicken is talking
+You catch me
 """
