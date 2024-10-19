77d5d15e722e515c6c8ab7c11a48dbdeb0539ba5
Vishal Pandey <bishalpandey2001@gmail.com>
2024-10-19 13:38:38 +0530

    Update solutions4.py

--- Problem Solving in Python/01_Basic Problems/Solutions/solutions4.py
+++ Problem Solving in Python/01_Basic Problems/Solutions/solutions4.py
@@ -1,3 +1,31 @@
+# Armstrong number: Approach 1
+number = int(input("Enter the number: "))
+original_number = number
+sum_of_powers = 0
+exponent = 0
+
+# To find the digits in the number
+temp = number
+while(temp > 0):
+    temp //= 10
+    exponent += 1
+
+# Lets perform the sum of the powers
+temp = number
+while(temp > 0):
+    digit = temp % 10
+    sum_of_powers += digit ** exponent
+    temp //= 10
+
+# Conditional Setup 
+if(sum_of_powers == original_number):
+    print("This number is Armstrong Number")
+else:
+    print("This number is not an Armstrong Number")
+
+
+# Armstrong number: Approach 2
+"""
 number = int(input("Enter the number: "))
 numStr = str(number)
 exponent = len(numStr)
@@ -8,6 +36,7 @@ if(sum == number):
     print("This number is Armstrong Number")
 else:
     print("This number is not an Armstrong Number")
-
+"""
+# Time complexity for Second Approach
 # Complexity: O(logn)
 # Space Complexity: O(1)  # because we are using a constant amount of space. 
